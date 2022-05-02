4e0a7e1dc43132cb5150d82ea03e6b4e51a71ad4
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-05-03 00:22:56 +0200

    finding and setting draggable tiles

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const gameboard = document.querySelector(".gameboard");
 const tiles = document.querySelectorAll(".tile");
 
 let tilesArray = [];
+let draggingTileIndex;
+let dragoverTileIndex;
+let rowsCount = 3;
+let colsCount = 3;
 
 class Tile {
   constructor(row, col, id) {
     this.id = id;
-    this.startRow = row;
-    this.startCol = col;
     this.curRow = row;
     this.curCol = col;
   }
 
   placeTile() {
-    // const thisTile = document.querySelector(`[data-id="${this.id}"]`);
-    // thisTile.style.gridRow = this.curRow;
-    // thisTile.style.gridColumn = this.curCol;
-
     document.querySelector(
       `[data-id="${this.id}"]`
     ).style.gridArea = `${this.curRow}/${this.curCol}/${this.curRow}/${this.curCol}`;
@@ -26,7 +24,7 @@ class Tile {
 function addTilesToDOM() {
   let displayedTiles = tilesArray.map((tile) => {
     tile = `
-    <div class="tile" data-id="${tile.id}" data-startrow="${tile.startRow}" data-startcol="${tile.startCol}" draggable="true"></div>
+    <div class="tile" data-id="${tile.id}" draggable="false"></div>
     `;
 
     return tile;
@@ -47,17 +45,62 @@ function tileSwap(firstTile, secondTile) {
   secondTile.placeTile();
 }
 
-// gameboard.addEventListener()
+gameboard.addEventListener("dragstart", (e) => {
+  draggingTileIndex = e.target.dataset.id;
+});
 
+gameboard.addEventListener("dragover", (e) => {
+  if (e.target.classList.contains("slider-tile")) {
+    dragoverTileIndex = e.target.dataset.id;
+  } else {
+    dragoverTileIndex = draggingTileIndex;
+  }
+});
 
+gameboard.addEventListener("dragend", (e) => {
+  tileSwap(tilesArray[dragoverTileIndex], tilesArray[draggingTileIndex]);
+});
 
-tilesArray.push(new Tile(1, 1, 1));
-tilesArray.push(new Tile(1, 2, 2));
+function setDragableTiles() {
+  const possibleMoves = [
+    [-1, 0],
+    [0, +1],
+    [+1, 0],
+    [0, -1],
+  ];
+  const sliderId = document.querySelector(".slider-tile").dataset.id;
+  const sliderCoords = [
+    tilesArray[sliderId].curRow,
+    tilesArray[sliderId].curCol,
+  ];
+
+  let draggableTilesId = tilesArray.reduce((acc, tile) => {
+    for (i = 0; i < possibleMoves.length; i++) {
+      if (
+        tile.curRow === sliderCoords[0] + possibleMoves[i][0] &&
+        tile.curCol === sliderCoords[1] + possibleMoves[i][1]
+      )
+        acc.push(tile.id);
+    }
+    return acc;
+  }, []);
+
+  document.querySelectorAll(".tile").forEach((tile) => {
+    tile.setAttribute("draggable", "false");
+  });
+
+  draggableTilesId.forEach((id) => {
+    document
+      .querySelector(`[data-id="${id}"]`)
+      .setAttribute("draggable", "true");
+  });
+}
+
+tilesArray.push(new Tile(1, 1, 0));
+tilesArray.push(new Tile(1, 2, 1));
 
 addTilesToDOM();
 
-// tileSwap(tilesArray[1],tilesArray[0])
+document.querySelector(`[data-id="0"]`).classList.add("slider-tile");
 
-// tilesArray[0].curRow = 2;
-// tilesArray[0].curCol = 2;
-// tilesArray[0].placeTile();
+setDragableTiles();
